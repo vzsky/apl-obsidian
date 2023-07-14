c0184d8012c68c73d141a1f2fb784f01f6e53cdb
vzsky <my999n@gmail.com>
2023-07-15 00:31:33 +0700

    initial

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { Plugin } from "obsidian";
+import { SYMBOL } from "./symbols";
+
+export default class ExamplePlugin extends Plugin {
+
+  key = "`"
+
+  APLTranslate (source: string): string{
+    if (source.length == 0) return ''
+    if (source[0] == this.key) {
+      return ( SYMBOL[source[1]] ?? source[0] + source[1] ) + this.APLTranslate(source.slice(2))
+    }
+    return source[0] + this.APLTranslate(source.slice(1))
+  }
+
+  async onload() {
+    this.registerMarkdownCodeBlockProcessor("apl", (source: string, el: HTMLElement, _) => {
+      let cls = "language-apl"
+      let text = this.APLTranslate(source)
+      el.createEl("pre", { cls }).createEl("code", { cls, text })
+    })
+  }
+}
